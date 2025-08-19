aa068a31a9e2ca61dc4d2973cf67f1a0c6b6ac6c
Martin Larralde <martin.larralde@embl.de>
2025-08-19 02:18:24 +0200

    Move `CandidateSet` private type to a dedicated file

---
+++ src/CandidateSet.c
@@ -0,0 +1,44 @@
+// ==================================================================
+// CandidateSet.h
+// Copyright (c) Jonathan Barker, 2002
+// Copyright (c) Martin Larralde, 2025
+// ==================================================================
+// Implementation of type CandidateSet.
+// ==================================================================
+
+#include <stdlib.h>
+
+#include "CandidateSet.h"
+#include "Molecule.h"
+#include "Template.h"
+
+// ==================================================================
+// Methods of local type CandidateSet
+// ==================================================================
+
+CandidateSet *CandidateSet_create(const Molecule *M)
+{
+	CandidateSet *S;
+	Atom *A;
+	int n = Molecule_count(M);
+	int m;
+
+	S = (CandidateSet*)calloc(1,sizeof(CandidateSet));
+    if(!S) return NULL;
+
+	S->atom=(Atom**)calloc(n,sizeof(Atom*));
+    
+    return S;
+}
+
+void CandidateSet_free(CandidateSet *S)
+{
+	if(S)
+	{
+		if(S->atom) free(S->atom);
+		if(S->coord) free(S->coord);
+		free(S);
+	}
+}
+
+// ==================================================================
